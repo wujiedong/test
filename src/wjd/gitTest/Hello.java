4c0078b940cfcfa7d9270da319ba3c8f9651c840
wjd <584399230@qq.com>
2016-03-17 16:44:37 +0800

    1

--- src/wjd/gitTest/Hello.java
+++ src/wjd/gitTest/Hello.java
@@ -3,5 +3,8 @@ package wjd.gitTest;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("11hello git");
+		System.out.println("11hello git");
+		String a = "11";
+		System.out.println(a);
 	}
 }
